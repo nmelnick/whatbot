c5de993a9115c783d8662fa79b742824c689d9c7
Michael Gillis <mgillis@gmail.com>
2009-01-03 01:08:16 +0000

    random! now with testing.

--- lib/whatbot/Command/KarmaHistory.pm
+++ lib/whatbot/Command/KarmaHistory.pm
@@ -25,7 +25,7 @@ sub random : GlobalRegEx('^(\w+) (like|hate)s what') {
 	my $nick = $message->from;
 	my $op	 = ($verb eq 'likes' ? 1 : -1);
 
-	my $karmas = $self->store->retrieve('karma', [ 'subject' ], { 'user' => $who, 'amount' => $op }, "ORDER BY rand()");
+	my $karmas = $self->store->retrieve('karma', [ 'subject' ], { 'user' => $who, 'amount' => $op }, "random()");
 
 	if (!$karmas or !@$karmas) {
 		return "$nick: I don't know what $who ${verb}s.";
