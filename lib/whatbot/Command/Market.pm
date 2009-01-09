bb82000183da90b7c92cf4c227a632ba28b0f958
Michael Gillis <mgillis@gmail.com>
2009-01-09 23:50:37 +0000

    rearrangement. plus, the exciting... stockrep

--- lib/whatbot/Command/Market.pm
+++ lib/whatbot/Command/Market.pm
@@ -19,66 +19,135 @@ has 'quote' => (
 	default => sub { Finance::Quote->new; }
 );
 
+has 'default_exchange' => (
+	is		=> 'ro',
+	isa		=> 'Str',
+	default => 'nyse'
+);
+
 sub register {
 	my ( $self ) = @_;
 	
 	$self->command_priority('Extension');
 	$self->require_direct(0);
-    $self->quote->timeout(15);
-    $self->quote->require_labels(qw/last p_change name/);
+	$self->quote->timeout(15);
+	$self->quote->require_labels(qw/last p_change name/);
+}
+
+sub process {
+	my $self = shift;
+	my $stocks = shift;
+	my $fields = shift;
+	my $format = shift;
+	
+	# default format is:
+	# symbol (name) allotherfields spaceseparated
+	if (!defined($format)) {
+		my @formatfields = grep {$_ ne 'name'} @$fields;
+		
+		$format = "%s";
+		if (@formatfields != @$fields) {
+			$format .= " (%s)";
+		}
+		$format .=  (" %s" x @formatfields);
+	}
+	
+	my $info = $self->quote->fetch($self->default_exchange, @$stocks);
+	return undef unless $info;
+	
+	my @out;
+	
+	foreach my $symbol (@$stocks) {
+		# this API is weird and involves commas in hash keys...
+		# I guess this is the old way of doing things (?)
+		
+		if (! $info->{$symbol,"success"}) {
+			push @out, $info->{$symbol,"errormsg"};
+			next;
+		}
+		
+		my %data;
+		
+		foreach my $field (@$fields) {
+			my $value = $info->{$symbol,$field};
+			
+			if ($field eq "p_change") {
+				$value = String::IRC->new("$value%");
+				if ($value =~ /^\-/) {
+					$value->red;
+				} else {
+					$value->green;
+				}
+			} elsif ($field eq "eps" && $value =~ /^-/) {
+				$value = String::IRC->new("$value");
+				$value->red;
+			}
+			$data{$field} = $value;
+		}
+		
+		push @out, sprintf($format, $symbol, map { $data{$_} } @$fields);
+	}
+	
+	return join(" - ", @out);
+}
+
+sub do_currency {
+	my $self = shift;
+	my $target = shift;
+	
+	my ( $to_cur, $from_cur ) = ( $target =~ m!^([a-z]+)/([a-z]+)$!io );
+	
+	if ( !defined($to_cur) || !defined($from_cur) ) {
+		return "wtf is $target";
+	}
+	
+	my $rate = $self->quote->currency($from_cur, $to_cur);
+	
+	if (!$rate) {
+		return "I can't get a rate for $from_cur/$to_cur.";
+	}
+	return "1 $from_cur == $rate $to_cur";
+}
+
+sub detail : GlobalRegEx('^stockrep (.+)$') {
+	my ( $self, $message, $captures ) = @_;
+	
+	my $target = $captures->[0];
+
+	# from here on we're dealing with stocks
+	my @stocks = map { s/\s//g; uc } split /,/, $target;
+	
+	my $detail_fields = [qw(name eps year_range div div_yield volume)];
+	my $format = "%s - %s  eps %s - last 52w: %s - div %s (%s%y) - vol %s";
+	
+	my $results = $self->process(\@stocks, $detail_fields, $format);
+	
+	if (!$results) {
+		return "I couldn't find anything for $target.";
+	}
+	
+	return $results;
 }
 
 sub parse_message : CommandRegEx('(.+)') {
 	my ( $self, $message, $captures ) = @_;
-
+	
 	my $target = $captures->[0];
-    return "what" unless ($target);
+	
+	if ( $target =~ m!/!o ) {
+		return $self->do_currency($target);
+	}
+	 
+	# from here on we're dealing with stocks
+	my @stocks = map { s/\s//g; uc } split /,/, $target;
+	
+	my $results = $self->process(\@stocks, [qw(name last p_change)]);
+	
+	if (!$results) {
+		return "I couldn't find anything for $target.";
+	}
 
-    if ( $target =~ m!/!o ) {
-        my ( $to_cur, $from_cur ) = ( $target =~ m!^([a-z]+)/([a-z]+)$!io );
-
-        if ( !defined($to_cur) || !defined($from_cur) ) {
-            return "wtf is $target";
-        }
-        
-        my $rate = $self->quote->currency($from_cur, $to_cur);
-
-        if (!$rate) {
-            return "I can't get a rate for $from_cur/$to_cur.";
-        }
-        return "1 $from_cur == $rate $to_cur";
-    }
-     
-    # from here on we're dealing with stocks
-	my ($exchange, $stocklist) = ($target =~ m/^(?:([^:]+):)?([^:]+)$/);
-    my @stocks = map { uc } split /,/, $stocklist;
-    
-    $exchange = "nyse" unless defined($exchange);
-    my $info = $self->quote->fetch($exchange, @stocks);
-
-    if (!$info) {
-        return "I couldn't find anything for $target.";
-    }
-
-    my @results;
-    
-    foreach my $symbol (@stocks) {
-        # this API is gay
-
-        if (! $info->{$symbol,"success"}) {
-            push @results, "$symbol: " . $info->{$symbol,"errormsg"};
-            next;
-        }
-
-        my ($corpname, $lastprice, $pchange) = map{ $info->{$symbol,$_} } qw(name last p_change);
-
-        my $pchangestr = String::IRC->new("$pchange%");
-        $pchangestr =~ /^\-/ ? $pchangestr->red : $pchangestr->green;
-
-        push @results, "$symbol ($corpname) $lastprice $pchangestr";
-    }
-
-    return join(" - ", @results);
+	return $results;
 }
 
 1;
