e1efcbe22337ad5391fbb370e24ab1bc4a17c38a
Nicholas Melnick <nick@abstractwankery.com>
2013-06-19 13:39:39 -0500

    Add a Task::whatbot to install everything

---
+++ Task-whatbot/lib/Task/whatbot.pm
@@ -0,0 +1,38 @@
+package Task::whatbot;
+use strict;
+use warnings;
+
+our $VERSION = '0.12';
+
+1;
+
+=pod
+
+=head1 NAME
+
+Task::whatbot - Installs whatbot and all of the modules provided by whatbot.org.
+
+=head1 DESCRIPTION
+
+Task::whatbot will install whatbot, as well as all of the modules originally
+provided by whatbot.org for a given release.
+
+=head1 CONTENTS
+
+=head3 L<whatbot>
+
+=head3 L<whatbot::Command::Blackjack>
+
+=head3 L<whatbot::Command::Market>
+
+=head3 L<whatbot::Command::RSS>
+
+=head3 L<whatbot::Command::Trade>
+
+=head3 L<whatbot::Command::Translate>
+
+=head1 LICENSE/COPYRIGHT
+
+Be excellent to each other and party on, dudes.
+
+=cut
