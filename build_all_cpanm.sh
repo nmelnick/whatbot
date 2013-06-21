a2afd242942e0e2829eb43cf36f4fb49366cc3fd
Nicholas Melnick <nick@abstractwankery.com>
2013-06-21 15:49:55 -0500

    Add cpanm builder

---
+++ build_all_cpanm.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+for module_name in whatbot \
+                   whatbot-Command-Annoying \
+                   whatbot-Command-Blackjack \
+                   whatbot-Command-Excuse \
+                   whatbot-Command-Market \
+                   whatbot-Command-Nickometer \
+                   whatbot-Command-PageRank \
+                   whatbot-Command-Paste \
+                   whatbot-Command-Quote \
+                   whatbot-Command-RSS \
+                   whatbot-Command-Trade \
+                   whatbot-Command-Translate \
+                   whatbot-Command-Weather \
+                   whatbot-IO-AIM; do
+    cd $module_name
+    make clean
+    cpanm .
+    cd ..
+done
