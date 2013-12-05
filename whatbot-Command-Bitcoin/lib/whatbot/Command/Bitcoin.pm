a73f0d213d7b2438ad57046fe355ee69f0b90ca2
Nicholas Melnick <nick@abstractwankery.com>
2013-12-05 13:19:53 -0600

    Add Bitcoin checker, using preev

---
+++ whatbot-Command-Bitcoin/lib/whatbot/Command/Bitcoin.pm
@@ -0,0 +1,93 @@
+###########################################################################
+# whatbot/Command/Bitcoin.pm
+###########################################################################
+# the whatbot project - http://www.whatbot.org
+###########################################################################
+
+package whatbot::Command::Bitcoin;
+use Moose;
+BEGIN { extends 'whatbot::Command' }
+
+use JSON ();
+use LWP::UserAgent ();
+use HTML::Entities qw(decode_entities);
+use HTML::Strip;
+use namespace::autoclean;
+
+our $VERSION = '0.1';
+
+has 'ua' => (
+	is		=> 'ro',
+	isa		=> 'LWP::UserAgent',
+	default => sub { LWP::UserAgent->new; }
+);
+
+has 'htmlstrip' => (
+	is 		=> 'ro',
+	isa 	=> 'HTML::Strip',
+	default	=> sub { HTML::Strip->new; }
+);
+
+sub register {
+	my ( $self ) = @_;
+	
+	$self->command_priority('Extension');
+	$self->require_direct(0);
+}
+
+sub parse_message : GlobalRegEx('^(?i)bitcoin( \w+)?') {
+	my ( $self, $message, $captures ) = @_;
+
+	my $currency = lc( $captures->[0] or 'USD' );
+	$currency =~ s/ //g;
+	my $pricing = $self->_get_prices($currency);
+	return 'Unable to reach server, sorry.' unless ($pricing);
+	return $pricing unless ( ref($pricing) );
+	return '1 BTC is worth ' . $self->_average_pricing($pricing) . ' ' . uc($currency) . '.';
+}
+
+sub _get_prices {
+	my ( $self, $currency ) = @_;
+
+	my $url = 'http://preev.com/pulse/source:bitstamp,btce,localbitcoins,mtgox/unit:btc,%s';
+	my $response = $self->ua->get( sprintf( $url, $currency ) );
+	if ( $response->is_success ) {
+		return 'Invalid currency, maybe?' unless ( $response->decoded_content() );
+		my $pricing = JSON::from_json( $response->decoded_content() );
+		return $pricing if ($pricing);
+	}
+	return;
+}
+
+sub _average_pricing {
+	my ( $self, $pricing_object ) = @_;
+
+	my $markets = $pricing_object->{'markets'};
+	my $market_count = 0;
+	my $total = 0;
+	foreach my $market ( keys %$markets ) {
+		$total += sprintf( "%.10g", $markets->{$market}->{'price'} );
+		$market_count++;
+	}
+	return ( $total / $market_count );
+}
+
+sub help {
+    return 'Bitcoin checks the value of bitcoin to the given currency, or USD.';
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+=pod
+
+=head1 NAME
+
+whatbot::Command::Bitcoin - Checks value of Bitcoin.
+
+=head1 LICENSE/COPYRIGHT
+
+Be excellent to each other and party on, dudes.
+
+=cut
---
+++ whatbot-Command-Bitcoin/t/bitcoin.t
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+use whatbot::Test;
+use JSON ();
+
+use_ok( 'whatbot::Command::Bitcoin', 'Load Module' );
+
+my $test = whatbot::Test->new();
+my $base_component = $test->get_base_component();
+
+ok( my $bitcoin = whatbot::Command::Bitcoin->new({
+	'base_component' => $base_component,
+	'my_config'      => {},
+	'name'           => 'Bitcoin',
+}), 'new' );
+
+$bitcoin->register();
+
+my $pricing = '{"markets":{"bitstamp":{"price":"1.029e+03","vol":"5.429e+04"},"btce":{"price":"1.002e+03","vol":"5.852e+04"},"localbitcoins":{"price":"1.086e+03","vol":"3.452e+02"},"mtgox":{"price":"1.098e+03","vol":"5.563e+04"}},"slot":null}';
+my $pricing_object = JSON::from_json($pricing);
+is( $bitcoin->_average_pricing($pricing_object), ( ( 1029 + 1002 + 1086 + 1098 ) / 4 ) );
+
+done_testing();
+
+1;
