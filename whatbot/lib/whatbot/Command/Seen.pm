2e562952dd7b4ec47a01e2c415879e22c88e69c2
Nicholas Melnick <nick@abstractwankery.com>
2014-01-15 11:10:47 -0600

    Seen is now alias aware

--- whatbot/lib/whatbot/Command/Seen.pm
+++ whatbot/lib/whatbot/Command/Seen.pm
@@ -24,7 +24,7 @@ sub store_user : Monitor {
 	my ( $self, $message ) = @_;
 	
 	return if ( $message->invisible );
-	$self->model('seen')->seen( lc( $message->from ), $message->content );
+	$self->model('Seen')->seen( lc( $message->from ), $message->content );
 	return;
 }
 
@@ -34,13 +34,29 @@ sub seen : CommandRegEx('(.+)') {
 	if ($captures) {
 		my $user = $captures->[0];
 		$user =~ s/[\?\!\.]+$//;
-		my $ret = $self->model('seen')->seen( lc($user) );
-		if ( $ret and $ret->user ) {
+		my @users = ($user);
+		my $aliases = $self->model('UserAlias')->related_users( lc($user) );
+		push( @users, @$aliases ) if ($aliases);
+
+		my $last;
+		foreach my $user_alias (@users) {
+			my $ret = $self->model('Seen')->seen( lc($user_alias) );
+			if ( $ret and $ret->user ) {
+				if ( not $last ) {
+					$last = $ret;
+				} elsif ( $ret->timestamp > $last->timestamp ) {
+					$last = $ret;
+				}
+			}
+		}
+
+		if ($last) {
 			return join(" ",
 				$user,
-				'was last seen on ',
-				strftime('%Y-%m-%d at %H:%M:%S', localtime( $ret->timestamp )),
-				'saying, "' . $ret->message . '".'
+				'was last seen' . ( $last->user ne lc($user) ? ' as ' . $last->user : '' ),
+				'on',
+				strftime('%Y-%m-%d at %H:%M:%S %Z', localtime( $last->timestamp )),
+				'saying, "' . $last->message . '".'
 			);
 		} else {
 			return 'I have not seen ' . $user . ' yet.';
