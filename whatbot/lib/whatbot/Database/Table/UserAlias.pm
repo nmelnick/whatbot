7c7fd146fdf32e73a03afe873e61d49e4bb33fc1
Nicholas Melnick <nick@abstractwankery.com>
2014-01-17 12:15:53 -0600

    Update formatting for UserAlias.pm

--- whatbot/lib/whatbot/Database/Table/UserAlias.pm
+++ whatbot/lib/whatbot/Database/Table/UserAlias.pm
@@ -1,11 +1,29 @@
 ###########################################################################
 # whatbot/Database/Table/UserAlias.pm
-###########################################################################
 # the whatbot project - http://www.whatbot.org
 ###########################################################################
 
 use MooseX::Declare;
 use Method::Signatures::Modifiers;
+
+=head1 NAME
+
+whatbot::Database::Table::UserAlias - Track user aliases
+
+=head1 SYNOPSIS
+
+ use whatbot::Database::Table::UserAlias;
+
+=head1 DESCRIPTION
+
+whatbot::Database::Table::UserAlias tracks a one user to many aliases
+relationship.
+
+=head1 METHODS
+
+=over 4
+
+=cut
 
 class whatbot::Database::Table::UserAlias extends whatbot::Database::Table {
     method BUILD(...) {
@@ -29,6 +47,13 @@ class whatbot::Database::Table::UserAlias extends whatbot::Database::Table {
         });
     }
 
+=item alias( $user, $alias )
+
+Set a new alias for a user. If a user isn't being tracked, it will start being
+tracked. If the alias already exists, this is a no-op. Returns true on success.
+
+=cut
+
     method alias( Str $user, Str $alias ) {
         $user = lc($user);
         $alias = lc($alias) or return;
@@ -46,6 +71,13 @@ class whatbot::Database::Table::UserAlias extends whatbot::Database::Table {
         return;
     }
 
+=item user_for_alias( $alias )
+
+Return a username attached to the given alias. Returns nothing if the alias is
+not found.
+
+=cut
+
     method user_for_alias( Str $alias ) {
         my $row = $self->search_one({
             'alias' => $alias,
@@ -56,6 +88,12 @@ class whatbot::Database::Table::UserAlias extends whatbot::Database::Table {
         return;
     }
 
+=item aliases_for_user( $user )
+
+Return an arrayref of aliases for the given user.
+
+=cut
+
     method aliases_for_user( Str $user ) {
         my $rows = $self->search({
             'user' => $user,
@@ -65,6 +103,13 @@ class whatbot::Database::Table::UserAlias extends whatbot::Database::Table {
         }
         return;
     }
+
+=item related_users( $user_or_alias )
+
+Retrieve all related users for this string, which could be other aliases or
+an attached user.
+
+=cut
 
     method related_users( Str $user_or_alias ) {
         my @users;
@@ -79,10 +124,24 @@ class whatbot::Database::Table::UserAlias extends whatbot::Database::Table {
         return \@users;
     }
 
+=item canonical_user( $user_or_alias )
+
+Like user_for_alias, returns a username attached to the given alias if one
+exists, but returns the alias if a parent username is not found.
+
+=cut
+
     method canonical_user( Str $user ) {
         my $lcuser = lc($user) or return;
         return ( $self->user_for_alias($lcuser) or $user );
     }
+
+=item remove( $user, $alias? )
+
+Remove an alias from a user. If no alias is provided, removes everything for
+that user. Returns true on success.
+
+=cut
 
     method remove( Str $user, Str $alias? ) {
         $user = lc($user);
@@ -114,72 +173,6 @@ class whatbot::Database::Table::UserAlias extends whatbot::Database::Table {
 1;
 
 =pod
-
-=head1 NAME
-
-whatbot::Database::Table::UserAlias - Track user aliases
-
-=head1 SYNOPSIS
-
- use whatbot::Database::Table::UserAlias;
-
-=head1 DESCRIPTION
-
-whatbot::Database::Table::UserAlias tracks a one user to many aliases
-relationship.
-
-=head1 METHODS
-
-=over 4
-
-=item alias( $user, $alias )
-
-Set a new alias for a user. If a user isn't being tracked, it will start being
-tracked. If the alias already exists, this is a no-op. Returns true on success.
-
-=item user_for_alias( $alias )
-
-Return a username attached to the given alias. Returns nothing if the alias is
-not found.
-
-=item aliases_for_user( $user )
-
-Return an arrayref of aliases for the given user.
-
-=item related_users( $user_or_alias )
-
-Retrieve all related users for this string, which could be other aliases or
-an attached user.
-
-=item canonical_user( $user_or_alias )
-
-Like user_for_alias, returns a username attached to the given alias if one
-exists, but returns the alias if a parent username is not found.
-
-=item remove( $user, $alias? )
-
-Remove an alias from a user. If no alias is provided, removes everything for
-that user. Returns true on success.
-
-=back
-
-=head1 INHERITANCE
-
-=over 4
-
-=item whatbot::Component
-
-=over 4
-
-=item whatbot::Database::Table
-
-=over 4
-
-=item whatbot::Database::Table::Soup
-
-=back
-
-=back
 
 =back
 
