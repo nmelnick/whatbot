d67840d9263b81d20049810724a7f38f61223bb8
Nicholas Melnick <nick@abstractwankery.com>
2014-02-03 16:48:37 -0600

    Reformat perldoc in Progress.pm

--- whatbot/lib/whatbot/Progress.pm
+++ whatbot/lib/whatbot/Progress.pm
@@ -1,52 +1,10 @@
 ###########################################################################
-# whatbot/Progress.pm
-###########################################################################
-# progress bar for whatbot
-###########################################################################
+# Progress.pm
 # the whatbot project - http://www.whatbot.org
 ###########################################################################
 
 use MooseX::Declare;
 use Method::Signatures::Modifiers;
-
-class whatbot::Progress {
-    has 'max'              => ( is => 'rw', isa => 'Int' );
-    has 'restrict_updates' => ( is => 'rw', isa => 'Int' );
-    has 'show_count'       => ( is => 'rw', isa => 'Int' );
-
-    method update ( Int $current ) {
-        return if ( $self->restrict_updates and $current % $self->restrict_updates != 0 );
-        return unless ( $self->max and $self->max > 0 );
-
-        my $pct = int( ( $current / $self->max ) * 100 );
-        my $line = '[';
-        for ( my $c = 0; $c < int($pct * 0.7); $c++ ) {
-            $line .= '=';
-        }
-        for ( my $c = 0; $c < (65 - int($pct * 0.65)); $c++ ) {
-            $line .= '-';
-        }
-        $line .= '] ' . $pct . '% ';
-        if ( $self->show_count ) {
-            $line .= $current . '/' . $self->max;
-        }
-        for ( my $c = 0; $c < ( 80 - length($line) ); $c++ ) {
-            $line .= ' ';
-        }
-        $line .= "\r";
-        print $line;
-    }
-
-    method finish {
-        $self->restrict_updates(0);
-        $self->update( $self->max );
-        print "\n";
-    }
-}
-
-1;
-
-=pod
 
 =head1 NAME
 
@@ -96,9 +54,59 @@ depending on the value of
 
 =over 4
 
-=item update
+=cut
 
-Update the progress bar with the given value
+class whatbot::Progress {
+    has 'max'              => ( is => 'rw', isa => 'Int' );
+    has 'restrict_updates' => ( is => 'rw', isa => 'Int' );
+    has 'show_count'       => ( is => 'rw', isa => 'Int' );
+
+=item update($value)
+
+Update the progress bar with the given value.
+
+=cut
+
+    method update ( Int $current ) {
+        return if ( $self->restrict_updates and $current % $self->restrict_updates != 0 );
+        return unless ( $self->max and $self->max > 0 );
+
+        my $pct = int( ( $current / $self->max ) * 100 );
+        my $line = '[';
+        for ( my $c = 0; $c < int($pct * 0.7); $c++ ) {
+            $line .= '=';
+        }
+        for ( my $c = 0; $c < (65 - int($pct * 0.65)); $c++ ) {
+            $line .= '-';
+        }
+        $line .= '] ' . $pct . '% ';
+        if ( $self->show_count ) {
+            $line .= $current . '/' . $self->max;
+        }
+        for ( my $c = 0; $c < ( 80 - length($line) ); $c++ ) {
+            $line .= ' ';
+        }
+        $line .= "\r";
+        print $line;
+    }
+
+=item finish()
+
+Finish the progress bar by setting to max, restricting updates, and setting a 
+newline.
+
+=cut
+
+    method finish {
+        $self->restrict_updates(0);
+        $self->update( $self->max );
+        print "\n";
+    }
+}
+
+1;
+
+=pod
 
 =back
 
