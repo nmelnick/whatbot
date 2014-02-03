85f6b32238d426507d094c14850177b7e92cc10f
Nicholas Melnick <nick@abstractwankery.com>
2014-02-03 12:58:18 -0600

    Add subroutine attribute documentation to Command.pm

--- whatbot/lib/whatbot/Command.pm
+++ whatbot/lib/whatbot/Command.pm
@@ -43,7 +43,94 @@ to hook into events.
 Each subroutine called by the Controller can return either a string or a 
 whatbot::Message.
 
+=head1 SUBROUTINE ATTRIBUTES
+
+A Command can respond to different types of messages, triggers, and events, and
+those are communicated via subroutine attributes. These attributes can be 
+combined for any individual method, or you can structure your code to have
+different entry points for each event. The available attributes are:
+
+=over 4
+
+=item Command
+
+Command is the most basic event type. The given method would fire when a message
+comes in with "<command-name> <method-name>". For example, for the command
+"Seen", and the method name "test", then it would fire if someone stated "seen
+test" in whatever context whatbot is listening.
+
+=item CommandRegEx('$regex')
+
+CommandRegEx is similar to Command, where it listens after the command name,
+except it will look for a match to the given regex instead of the method name.
+To respond to "seen test", you would provide CommandRegEx('test').
+
+=item GlobalRegEx('$regex')
+
+GlobalRegEx will fire if the given regex is found on any input, whether the
+command name is involved or not. This is useful for parsing any content in a
+message, or looking for a triggering keyword.
+
+=item Monitor
+
+Monitor will fire on any incoming, visible message.
+
+=item StopAfter
+
+Force whatbot to stop processing commands after this method returns.
+
+=item Event('$event')
+
+Event will fire on the given incoming event. To specify multiple events to
+respond to, multiple Event attributes must be provided. An Event attribute
+does result in a change to the method signature, as it will provide $self,
+$target, which is the context that the event was fired from, and $event_info,
+which is a hashref containing different data depending on the event. Events are
+provided by IO modules, so you will want to check those for additional event
+types. In general, the possible events are:
+
+=over 4
+
+=item enter : $event_info contains 'nick'
+
+=item user_change : $event_info contains 'nick', 'old_nick'
+
+=item leave : $event_info contains 'nick'
+
+=item ping : $event_info contains 'source'
+
+=item topic : $event_info contains 'nick', 'topic'
+
+=back
+
+=back
+
 =head1 PUBLIC ACCESSORS
+
+=over 4
+
+=item command_priority
+
+Determines at what point in the processing order this command will fire. Valid
+entries are 'Primary', 'Core', 'Extension', and defaults to Extension. Primary
+are first runners, Core are components considered essential, and Extension is
+parsed in order after those components.
+
+=item require_direct
+
+Forces the module to only respond if the name of the bot is used in the message.
+
+=item my_config
+
+Contains the configuration for this module from the whatbot config file, if any.
+
+=item timer
+
+Provides access to whatbot::Timer functionality.
+
+=back
+
+=head1 PUBLIC METHODS
 
 =over 4
 
@@ -53,53 +140,13 @@ class whatbot::Command extends whatbot::Component {
     use whatbot::Types qw( HTTPRequest );
 
     has 'name'             => ( is => 'rw', isa => 'Str' );
-
-=item command_priority
-
-Determines at what point in the processing order this command will fire. Valid
-entries are 'Primary', 'Core', 'Extension', and defaults to Extension. Primary
-are first runners, Core are components considered essential, and Extension is
-parsed in order after those components.
-
-=cut
-
-    has 'command_priority' => ( is => 'rw', isa => 'Str', default => 'Extension' );
-
-=item require_direct
-
-Forces the module to only respond if the name of the bot is used in the message.
-
-=cut
-
     has 'require_direct'   => ( is => 'rw', isa => 'Int', default => 0 );
-
-=item my_config
-
-Contains the configuration for this module from the whatbot config file, if any.
-
-=cut
     has 'my_config'        => ( is => 'ro', isa => 'Maybe[HashRef]' );
-
-=item timer
-
-Provides access to whatbot::Timer functionality.
-
-=cut
-
     has 'timer'            => ( is => 'rw', lazy_build => 1 );
 
     sub _build_timer {
         return $_[0]->ios->{Timer};
     }
-
-
-=back
-
-=head1 PUBLIC METHODS
-
-=over 4
-
-=cut
 
     our $_attribute_cache = {};
 
