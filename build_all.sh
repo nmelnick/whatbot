c985a24c07eb95494653431ce4b8007c7adbb414
Nicholas Melnick <nick@abstractwankery.com>
2014-10-17 21:30:11 +0000

    Clean up build_all scripts

--- build_all.sh
+++ build_all.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
 
-for module_name in whatbot \
-                   whatbot-Command-Annoying \
-                   whatbot-Command-Bitcoin \
-                   whatbot-Command-Blackjack \
-                   whatbot-Command-Excuse \
-                   whatbot-Command-Market \
-                   whatbot-Command-Nickometer \
-                   whatbot-Command-PageRank \
-                   whatbot-Command-Paste \
-                   whatbot-Command-Quote \
-                   whatbot-Command-RSS \
-                   whatbot-Command-Trade \
-                   whatbot-Command-Weather \
-                   whatbot-IO-AIM \
-                   whatbot-IO-Jabber; do
+for module_name in whatbot whatbot*/ ; do
     cd $module_name
     make clean
     perl Makefile.PL
--- build_all_cpanm.sh
+++ build_all_cpanm.sh
@@ -1,21 +1,7 @@
 #!/bin/sh
 
 cpanm --prompt Module::Install
-for module_name in whatbot \
-                   whatbot-Command-Annoying \
-                   whatbot-Command-Bitcoin \
-                   whatbot-Command-Blackjack \
-                   whatbot-Command-Dogecoin \
-                   whatbot-Command-Excuse \
-                   whatbot-Command-Market \
-                   whatbot-Command-Nickometer \
-                   whatbot-Command-PageRank \
-                   whatbot-Command-Paste \
-                   whatbot-Command-Quote \
-                   whatbot-Command-RSS \
-                   whatbot-Command-Weather \
-                   whatbot-IO-AIM \
-                   whatbot-IO-Jabber; do
+for module_name in whatbot whatbot*/ ; do
     cd $module_name
     rm Makefile
     cpanm --prompt .
