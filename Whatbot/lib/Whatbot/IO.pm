e9f911761450c147e484f6df7a75ffb8d637c375
Nicholas Melnick <nick@abstractwankery.com>
2016-08-30 13:56:05 -0500

    Add perldoc to Whatbot::IO

--- Whatbot/lib/Whatbot/IO.pm
+++ Whatbot/lib/Whatbot/IO.pm
@@ -8,6 +8,82 @@
 
 use Moops;
 
+=head1 NAME
+
+Whatbot::IO - Base class for Whatbot IO modules
+
+=head1 SYNOPSIS
+
+ use Moops;
+ use Whatbot::Command; # Import subroutine attributes
+ class Whatbot::IO::Example extends Whatbot::IO {
+
+   method BUILD (...) {
+     $self->name('Example');
+     $self->me( $self->my_config->{'nickname'} );
+   }
+
+   method connect() {}
+
+   method disconnect() {}
+
+   method send_message($message) {}
+ }
+
+=head1 DESCRIPTION
+
+Whatbot::IO is a base class, meant to be subclassed by an IO provider or engine
+to connect to an external chat/messaging service. It provides a skeleton
+structure to create a a provider, and methods to handle communication in and out
+of Whatbot.
+
+Keep in mind that if you're looking for functionality that you'd expect in your
+IO provider, and not seeing it here, double check L<Whatbot::Component>.
+
+Some terminology used:
+
+=over 4
+
+=item context
+
+This is the context in which this bot is chatting on this service. In IRC, this
+would be known as the 'channel', where other services may call it a 'room'.
+
+=item topic
+
+A description for a context, taken from the IRC concept of a topic.
+
+=item nick
+
+A username or nickname of an individual within the context, taken from IRC.
+
+=back
+
+=head1 PUBLIC ACCESSORS
+
+=over 4
+
+=item my_config
+
+The portion of the user's configuration file devoted to this IO module.
+
+=item name
+
+The name of this IO module, used in logging or looking up a provider to send a
+message through.
+
+=item me
+
+The name or nickname of this instance of Whatbot in this IO module.
+
+=item ignore
+
+A list of users or tags to ignore processing of.
+
+=back
+
+=cut
+
 class Whatbot::IO extends Whatbot::Component {
 	use Whatbot::Message;
 
@@ -33,32 +109,89 @@ class Whatbot::IO extends Whatbot::Component {
 		}
 	}
 
-	method notify ( Str $context, Str $message ) {
-		$self->log->write( sprintf( '(%s) [%s] %s', $self->name, $context, $message ) )
-			unless ( defined $self->my_config->{'silent'} );
-	}
+=head1 IMPLEMENTATION METHODS
+
+These methods are required to be implemented to provide a fully functional IO
+module for Whatbot.
+
+=over 4
+
+=item connect()
+
+Override this method. This method is called when Whatbot is connecting to this
+module, whether on first load, or after a disconnection.
+
+=cut
 
 	method connect {
 	}
 
+=item disconnect()
+
+Override this method. This method is called when Whatbot is attempting to
+disconnect from this module, generally when Whatbot is exiting.
+
+=cut
+
 	method disconnect {
 	}
 
-	method event_user_enter ( $context?, $from? ) {
-		$self->notify( $context, '** ' . $from . ' has entered' );
+=item send_message($message)
+
+Override this method. This method, given a Whatbot::Message, will send the
+message out through this service.
+
+=cut
+
+	method send_message( $message ) {
+		$self->log->error( ref($self) . ' does not know how to send_message' );
+	}
+
+=back
+
+=head1 PUBLIC METHODS
+
+=over 4
+
+=item notify($context, $message)
+
+Write a message to the log with clear indication that this module is the sender
+of that message.
+
+=cut
+
+	method notify( Str $context, Str $message ) {
+		$self->log->write( sprintf( '(%s) [%s] %s', $self->name, $context, $message ) )
+			unless ( defined $self->my_config->{'silent'} );
+	}
+
+=item event_user_enter($context, $nick)
+
+Send a Whatbot event that a user has entered the context.
+
+=cut
+
+	method event_user_enter( $context?, $nick? ) {
+		$self->notify( $context, '** ' . $nick . ' has entered' );
 		$self->parse_response(
 			$self->controller->handle_event(
 				join( ':', $self->name, $context ),
 				'enter',
 				{
-					'nick' => $from
+					'nick' => $nick
 				},
 				$self->me
 			)
 		);
 	}
 
-	method event_user_change ( $context?, $old_nick?, $new_nick? ) {
+=item event_user_change($context, $old_nick, $new_nick)
+
+Send a Whatbot event that a user has changed their name.
+
+=cut
+
+	method event_user_change( $context?, $old_nick?, $new_nick? ) {
 		$self->notify( $context, '** ' . $old_nick . ' is now ' . $new_nick );
 		$self->parse_response(
 			$self->controller->handle_event(
@@ -73,19 +206,31 @@ class Whatbot::IO extends Whatbot::Component {
 		);
 	}
 
-	method event_user_leave ( $context?, $from?, $message? ) {
-		$self->notify( $context, '** ' . $from . ' has left ' );
+=item event_user_leave($context, $nick)
+
+Send a Whatbot event that a user has left the context.
+
+=cut
+
+	method event_user_leave( $context?, $nick?, $message? ) {
+		$self->notify( $context, '** ' . $nick . ' has left ' );
 		$self->parse_response(
 			$self->controller->handle_event(
 				join( ':', $self->name, $context ),
 				'leave',
 				{
-					'nick' => $from
+					'nick' => $nick
 				},
 				$self->me
 			)
 		);
 	}
+
+=item event_ping($source)
+
+Send a Whatbot event that someone has sent a ping-like request to Whatbot.
+
+=cut
 
 	method event_ping ( $source? ) {
 		$self->parse_response(
@@ -99,6 +244,12 @@ class Whatbot::IO extends Whatbot::Component {
 			)
 		);
 	}
+
+=item event_topic($context, $topic, $who)
+
+Send a Whatbot event that the topic of this context has changed.
+
+=cut
 
 	method event_topic ( $context, $topic, $who ) {
 		$self->parse_response(
@@ -114,7 +265,14 @@ class Whatbot::IO extends Whatbot::Component {
 		);
 	}
 
-	method event_message ( Whatbot::Message $message ) {
+=item event_message($message)
+
+Send a Whatbot event that a message has been received. This will generally
+invoke one or more commands. The $message variable must be a Whatbot::Message.
+
+=cut
+
+	method event_message( Whatbot::Message $message ) {
 		$self->notify( $message->to, '<' . $message->from . '> ' . $message->content );
 		$message->me( $self->me );
 		$message->origin( join( ':', $self->name, ( $message->is_private ? $message->from : $message->to ) ) );
@@ -127,25 +285,37 @@ class Whatbot::IO extends Whatbot::Component {
 		}
 	}
 
-	method event_action ( Str $to, Str $from, Str $content ) {
+=item event_action($to, $from, $content)
+
+Send a Whatbot event that an action has been received. An action is a message
+that a user is doing something. In IRC, this is triggered when someone sends a
+/me.
+
+=cut
+
+	method event_action( Str $to, Str $from, Str $content ) {
 		$self->notify( $to, '[ACT] ' . $from . ' ' . $content );
 	}
 
-	method send_message ( $message ) {
-	}
+=item get_new_message(\%params)
 
-	method parse_response ( $messages ) {
-		$messages = [$messages] unless ( ref($messages) and ref($messages) eq 'ARRAY' );
-		foreach my $message ( @{$messages} ) {
-			$self->send_message($message);
-		}
-	}
+Get a new instance of a Whatbot::Message with a prefilled 'me', and then given
+$params, as one would pass into Whatbot::Message->new().
+
+=cut
 
 	method get_new_message( HashRef $params ) {
 		return Whatbot::Message->new({
 			'me' => $self->me,
 			%$params
 		})
+	}
+
+	method parse_response( $messages ) {
+		$messages = [$messages] unless ( ref($messages) and ref($messages) eq 'ARRAY' );
+		foreach my $message ( @{$messages} ) {
+			$self->send_message($message);
+		}
 	}
 
 	method _is_ignored_user( Str $user ) {
@@ -159,3 +329,13 @@ class Whatbot::IO extends Whatbot::Component {
 }
 
 1;
+
+=pod
+
+=back
+
+=head1 LICENSE/COPYRIGHT
+
+Be excellent to each other and party on, dudes.
+
+=cut
