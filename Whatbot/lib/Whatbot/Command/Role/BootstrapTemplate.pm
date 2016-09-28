3600a74b491f021f099afc947cdcf009f49c73e7
Nicholas Melnick <nick@abstractwankery.com>
2016-09-28 09:33:59 -0500

    Update Bootstrap to 3.3.7.

--- Whatbot/lib/Whatbot/Command/Role/BootstrapTemplate.pm
+++ Whatbot/lib/Whatbot/Command/Role/BootstrapTemplate.pm
@@ -102,7 +102,7 @@ string.
 
 	<title>[% page_title || 'whatbot' %]</title>
 
-	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 	<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -188,7 +188,7 @@ string.
 	method _footer_template() {
 		return q{</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
 };
